1a0937462e8beaceefa41f2f86458de68aeef32c
Aspiring-Writer <yolson@liberty.edu>
2021-12-15 18:13:43 -0400

    Update library design, add tag pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,13 @@ const slugify = require('@sindresorhus/slugify');
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/assets/covers");
+  eleventyConfig.addPassthroughCopy("src/assets/*.js")
 
   eleventyConfig.addFilter("slugify", slugify);
+  eleventyConfig.addFilter("strip_html", (book) => {
+    const content = book.replace(/(<([^>]+)>)/gi, "");
+    return content;
+  });
 
   return {
     htmlTemplateEngine: "njk",
