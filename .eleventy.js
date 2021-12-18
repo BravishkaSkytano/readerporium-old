8a0d642c10947272828359691058022364c3418c
Aspiring-Writer <yolson@liberty.edu>
2021-12-18 18:39:14 -0400

    Add sortByOrder filter, favicons, wordcount

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
 const slugify = require('@sindresorhus/slugify');
 
+function sortByOrder(values) {
+  let vals = [...values];     // this *seems* to prevent collection mutation...
+  return vals.sort((a, b) => Math.sign(a.order - b.order));
+}
+
 module.exports = function (eleventyConfig) {
 
-  eleventyConfig.addPassthroughCopy("src/assets/covers");
-  eleventyConfig.addPassthroughCopy("src/assets/*.js")
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   eleventyConfig.addFilter("slugify", slugify);
   eleventyConfig.addFilter("strip_html", (book) => {
     const content = book.replace(/(<([^>]+)>)/gi, "");
     return content;
   });
+  eleventyConfig.addFilter('sortByOrder', sortByOrder)
 
   return {
     htmlTemplateEngine: "njk",
